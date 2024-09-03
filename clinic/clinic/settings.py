17204e37c8a0a53a7316eee03d523bd81b476ec4
DivineUX23 <divineigbinoba23@gmail.com>
2024-09-03 18:10:37 +0100

    Updated

--- clinic/clinic/settings.py
+++ clinic/clinic/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 #from pathlib import Path
 import os
 from decouple import config
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -86,6 +87,7 @@ DATABASES = {
     }
 }
 
+DATABASES["default"] = dj_database_url.parse(config("DATABASES"))
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
